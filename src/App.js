d68c7adebbb2e17cf537fc2d11efab8e161c7972
Akshat <akshatsingla231@gmail.com>
2021-07-25 14:14:54 +0530

    Deployment ready

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.scss";
 import React from "react";
 import io from "socket.io-client";
 
-const socket = io.connect('http://localhost:8000/');
+const socket = io.connect('https://encrypted-chatapp.herokuapp.com/');
 
 function Appmain(props) {
   return (
